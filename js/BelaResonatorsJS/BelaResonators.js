d2ef56b30421a2067cee393fbbb2d784ca938834
Jack Armitage <jack.armitage@me.com>
2020-04-22 08:11:57 +0100

    Update JS API

--- js/BelaResonatorsJS/BelaResonators.js
+++ js/BelaResonatorsJS/BelaResonators.js
@@ -3,19 +3,10 @@ import Bela from './Bela.js'
 export default class BelaResonators extends Bela {
 	constructor(ip='192.168.7.2') { super(ip) }
 
-	setModel(model) {
+	setModel(index, model) {
 		if (this.isConnected()) {
 			this.control.send({ 
-				command: 'setModel', 
-				args: model 
-			})
-		}
-	}
-
-	setModelAtIndex(index, model) {
-		if (this.isConnected()) {
-			this.control.send({ 
-				command: 'setModel', 
+				command: 'updateModel',
 				args: {
 					index: index,
 					model: model
@@ -24,46 +15,16 @@ export default class BelaResonators extends Bela {
 		}
 	}
 
-	setResonatorParamAtBankIndex(bankIndex, resIndex, paramIndex, value) {
+	setPitch(index, pitch) {
 		if (this.isConnected()) {
 			this.control.send({ 
-				command: 'setResParamAtBankIndex', 
+				command: 'updatePitch',
 				args: {
-					bankIndex:  bankIndex, 
-					resIndex:   resIndex, 
-					paramIndex: paramIndex, 
-					value:      value
+					index: index,
+					pitch: pitch
 				}
 			})
 		}
 	}
-
-	setResonatorAtBankIndex(bankIndex, resIndex, params) {
-		if (this.isConnected()) {
-			this.control.send({ 
-				command: 'setResAtBankIndex', 
-				args: {
-					bankIndex: bankIndex, 
-					resIndex:  resIndex, 
-					params:    params
-				}
-			})
-		}
-	}
-
-  //   setResonatorsAtBankIndex(bankIndex, resIndices, paramVects) {
-  //   	// paramVects = [{f, g, d}];
-  //   	// paramVects = []
-  //   	if (this.isConnected()) {
-		// 	this.control.send({ 
-		// 		command: 'setRessAtBankIndex', 
-		// 		args: {
-		// 			bankIndex:  bankIndex, 
-		// 			resIndices: resIndices, 
-		// 			paramVects: paramVects
-		// 		}
-		// 	})
-		// }
-  //   }
 
 }
